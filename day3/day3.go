4688ebc3dc0c58ec2ea7c91ae7c3be409cf0ee2c
CiTuX <info@citux.ch>
2015-12-09 17:24:57 +0100

    Day 3 - Part 1

--- day3/day3.go
+++ day3/day3.go
@@ -5,8 +5,44 @@ import (
 	"../advent"
 )
 
+const north, south, east, west = '^', 'v', '>', '<'
+
+type Position struct {
+	x, y int
+}
+
 func main() {
 	input := advent.ReadInput("day3")
+	part1(input)
+}
 
-	fmt.Print(input)
+func part1(input string) {
+	presents := make(map[Position]int)
+
+	x := 0
+	y := 0
+
+	presents[Position{x, y}] = 1
+
+	for _, char := range input {
+
+		switch char {
+		case north:
+			y++
+			break
+		case south:
+			y--
+			break
+		case east:
+			x--
+			break
+		case west:
+			x++
+			break
+		}
+
+		presents[Position{x, y}]++
+		fmt.Print(x, "|", y, "=", presents[Position{x, y}], "\n")
+	}
+	fmt.Print("\n\n", len(presents))
 }
