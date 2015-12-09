2366ce5fe864fa6bfed23fd45c4fd425e33d5250
CiTuX <info@citux.ch>
2015-12-09 17:44:55 +0100

    Day 3 - Part 2

--- day3/day3.go
+++ day3/day3.go
@@ -14,6 +14,25 @@ type Position struct {
 func main() {
 	input := advent.ReadInput("day3")
 	part1(input)
+	part2(input)
+}
+
+func changePosition(char rune, x, y int) (int, int) {
+	switch char {
+	case north:
+		y++
+		break
+	case south:
+		y--
+		break
+	case east:
+		x--
+		break
+	case west:
+		x++
+		break
+	}
+	return x, y
 }
 
 func part1(input string) {
@@ -26,23 +45,39 @@ func part1(input string) {
 
 	for _, char := range input {
 
-		switch char {
-		case north:
-			y++
-			break
-		case south:
-			y--
-			break
-		case east:
-			x--
-			break
-		case west:
-			x++
-			break
-		}
+		x, y = changePosition(char, x, y)
 
 		presents[Position{x, y}]++
-		fmt.Print(x, "|", y, "=", presents[Position{x, y}], "\n")
 	}
-	fmt.Print("\n\n", len(presents))
+	fmt.Print("Part 1: ", len(presents))
+}
+
+func part2(input string) {
+	presents := make(map[Position]int)
+
+	//Santa
+	sX := 0
+	sY := 0
+
+	//Robo
+	rX := 0
+	rY := 0
+
+	//Init
+	presents[Position{sX, sY}] = 1
+	presents[Position{rX, rY}] = 1
+
+
+	for index, char := range input {
+		switch index % 2 {
+		case 0:
+			rX, rY = changePosition(char, rX, rY) //Robo
+			presents[Position{rX, rY}]++
+		case 1:
+			sX, sY = changePosition(char, sX, sY) //Santa
+			presents[Position{sX, sY}]++
+		}
+	}
+
+	fmt.Print("\nPart 2: ", len(presents))
 }
