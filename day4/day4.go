dbbc468259a9c9929463a42134c10a1d63c82be9
CiTuX <info@citux.ch>
2015-12-09 19:03:42 +0100

    Day 4 - Part 1

--- day4/day4.go
+++ day4/day4.go
@@ -1,10 +1,31 @@
 package main
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"crypto/md5"
+	"encoding/hex"
+)
 
 const input = "ckczppom"
+const requirement = "00000"
+
+var inputBytes = []byte(input)
 
 func main() {
 	part1()
 }
 
 func part1() {
+	var hash string
+
+	count := int64(0)
+	hasher := md5.New()
+	for (!strings.HasPrefix(hash, requirement)) {
+		count++
+		hasher.Reset()
+		hasher.Write(strconv.AppendInt(inputBytes, count, 10))
+		hash = hex.EncodeToString(hasher.Sum(nil))
+	}
+	fmt.Print(count, "\n", hash)
 }
