5bf27ae28e90bc6a7a6ab8489d1c00cfcdbf5f97
CiTuX <info@citux.ch>
2015-12-10 00:15:54 +0100

    Day 5 - Part 2

--- day5/day5.go
+++ day5/day5.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"../advent"
 	"strings"
+
+	"../advent"
 )
 
 var vowels = []string{"a", "e", "i", "o", "u"}
@@ -12,23 +13,85 @@ var blacklist = []string{"ab", "cd", "pq", "xy"}
 func main() {
 	input := advent.ReadInput()
 	part1(input)
+	fmt.Print("\n\n")
+	part2(input)
 }
 
 func part1(input string) {
 	var count int
 	for _, line := range strings.Split(input, "\n") {
-		if (checkWord(line)) {
+		if checkWordSimpel(line) {
 			count++
 		}
 	}
-	fmt.Print("Count: ", count)
+	fmt.Print("Part1: ", count)
 }
 
-func checkWord(word string) (nice bool) {
+func part2(input string) {
+	var count int
+	for _, line := range strings.Split(input, "\n") {
+		if checkWordKomplex(line) {
+			count++
+		}
+	}
+	fmt.Print("Part2: ", count)
+}
+
+func checkWordKomplex(word string) bool {
+	if (checkContainsPairTwice(word)) {
+		if checkRepeatLetter(word) {
+			return true
+		}
+	}
+	return false
+}
+
+func checkContainsPairTwice(word string) (ok bool) {
+	pairs := parsePairs(word)
+	if pairs != nil {
+		return hasDuplicates(pairs)
+	}
+	return false
+}
+
+func parsePairs(word string) (pairs []string) {
+	wordLen := len(word)
+	pairs = make([]string, wordLen)
+	for i := 0; i < wordLen - 1; i++ {
+		pairs[i] = word[i : i + 2]
+	}
+	return
+}
+
+func checkRepeatLetter(word string) bool {
+	wordLen := len(word)
+	for i := 0; i < wordLen - 3; i++ {
+		if word[i] == word[i + 2] {
+			return true
+		}
+	}
+	return false
+}
+
+func hasDuplicates(elements []string) bool {
+	for i := 0; i < len(elements); i++ {
+		// Scan slice for a previous element of the same value.
+		for v := 0; v < i; v++ {
+			if elements[v] == elements[i] {
+				if (i - v != 1) {
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+func checkWordSimpel(word string) (nice bool) {
 	nice = false
-	if (countVowels(word) >= 3) {
-		if (letterTwiceInRow(word)) {
-			if (checkBlacklist(word)) {
+	if countVowels(word) >= 3 {
+		if letterTwiceInRow(word) {
+			if checkBlacklist(word) {
 				nice = true
 			}
 		}
@@ -46,7 +109,7 @@ func countVowels(word string) (count int) {
 func letterTwiceInRow(word string) (twice bool) {
 	var lastLetter rune
 	for _, letter := range word {
-		if (letter == lastLetter) {
+		if letter == lastLetter {
 			return true
 		}
 		lastLetter = letter
@@ -56,7 +119,7 @@ func letterTwiceInRow(word string) (twice bool) {
 
 func checkBlacklist(word string) (ok bool) {
 	for _, entry := range blacklist {
-		if (strings.Contains(word, entry)) {
+		if strings.Contains(word, entry) {
 			return false
 		}
 	}
