83525da9efa304ffca366bf5b9c754ad1baa3d00
CiTuX <info@citux.ch>
2015-12-09 22:57:35 +0100

    Day 5 - Part 1

--- day5/day5.go
+++ day5/day5.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"../advent"
+	"strings"
 )
+
+var vowels = []string{"a", "e", "i", "o", "u"}
+var blacklist = []string{"ab", "cd", "pq", "xy"}
 
 func main() {
 	input := advent.ReadInput()
@@ -11,5 +15,50 @@ func main() {
 }
 
 func part1(input string) {
-	fmt.Print(input)
+	var count int
+	for _, line := range strings.Split(input, "\n") {
+		if (checkWord(line)) {
+			count++
+		}
+	}
+	fmt.Print("Count: ", count)
+}
+
+func checkWord(word string) (nice bool) {
+	nice = false
+	if (countVowels(word) >= 3) {
+		if (letterTwiceInRow(word)) {
+			if (checkBlacklist(word)) {
+				nice = true
+			}
+		}
+	}
+	return
+}
+
+func countVowels(word string) (count int) {
+	for _, vowel := range vowels {
+		count += strings.Count(word, vowel)
+	}
+	return count
+}
+
+func letterTwiceInRow(word string) (twice bool) {
+	var lastLetter rune
+	for _, letter := range word {
+		if (letter == lastLetter) {
+			return true
+		}
+		lastLetter = letter
+	}
+	return false
+}
+
+func checkBlacklist(word string) (ok bool) {
+	for _, entry := range blacklist {
+		if (strings.Contains(word, entry)) {
+			return false
+		}
+	}
+	return true
 }
