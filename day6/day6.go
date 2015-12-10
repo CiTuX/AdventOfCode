0acf8c17adf3ca39cec668afa5f50e020917189f
CiTuX <info@citux.ch>
2015-12-10 02:07:49 +0100

    Day 6 - Part 2

--- day6/day6.go
+++ day6/day6.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"../advent"
-	"strings"
-	"strconv"
 	"fmt"
+	"strconv"
+	"strings"
+
+	"../advent"
 )
 
 const turn, on, off, toggle, through, separator = "turn", "on", "off", "toggle", "through", ","
 
-var theGrid [1000][1000]bool
+var theLight [1000][1000]bool
+var theBright [1000][1000]int
 
 type Index struct {
 	x, y int
@@ -22,11 +24,13 @@ type Coords struct {
 func main() {
 	input := advent.ReadInput()
 	part1(input)
+	fmt.Print("\n\n")
+	part2(input)
 }
 
 func part1(input string) {
 	for _, line := range strings.Split(input, "\n") {
-		if (line != "") {
+		if line != "" {
 			coords := parseCoords(line)
 			state := parseState(line)
 			toogle := parseToggle(line)
@@ -38,7 +42,7 @@ func part1(input string) {
 	var counter int
 	for x := 0; x < 1000; x++ {
 		for y := 0; y < 1000; y++ {
-			if (theGrid[x][y]) {
+			if theLight[x][y] {
 				counter++
 			}
 		}
@@ -46,20 +50,56 @@ func part1(input string) {
 	fmt.Print("Part 1: ", counter)
 }
 
+func part2(input string) {
+	for _, line := range strings.Split(input, "\n") {
+		if line != "" {
+			coords := parseCoords(line)
+			state := parseState(line)
+			toogle := parseToggle(line)
+
+			setBrightness(coords, state, toogle)
+		}
+	}
+
+	var brightness int
+	for x := 0; x < 1000; x++ {
+		for y := 0; y < 1000; y++ {
+			brightness += theBright[x][y]
+		}
+	}
+	fmt.Print("Part 2: ", brightness)
+}
+
 func setLight(coords Coords, state, toggle bool) {
 	for x := coords.from.x; x <= coords.to.x; x++ {
 		for y := coords.from.y; y <= coords.to.y; y++ {
-			if (toggle) {
-				theGrid[x][y] = !theGrid[x][y]
-			}else {
-				theGrid[x][y] = state
+			if toggle {
+				theLight[x][y] = !theLight[x][y]
+			} else {
+				theLight[x][y] = state
+			}
+		}
+	}
+}
+
+func setBrightness(coords Coords, state, toggle bool) {
+	for x := coords.from.x; x <= coords.to.x; x++ {
+		for y := coords.from.y; y <= coords.to.y; y++ {
+			if toggle {
+				theBright[x][y] += 2
+			} else {
+				if (state) {
+					theBright[x][y] += 1
+				}else if (theBright[x][y] > 0) {
+					theBright[x][y] -= 1
+				}
 			}
 		}
 	}
 }
 
 func parseState(word string) bool {
-	if (strings.HasPrefix(word, turn)) {
+	if strings.HasPrefix(word, turn) {
 		switch word[5:7] {
 		case on:
 			return true
@@ -79,13 +119,13 @@ func parseCoords(line string) Coords {
 	var indexes []Index
 
 	for _, word := range strings.Split(line, " ") {
-		if (strings.Contains(word, separator)) {
+		if strings.Contains(word, separator) {
 			separated := strings.Split(word, separator)
 			indexes = append(indexes, parseIndex(separated))
 		}
 	}
 
-	if (len(indexes) == 2) {
+	if len(indexes) == 2 {
 		coords.from = indexes[0]
 		coords.to = indexes[1]
 	}
@@ -94,7 +134,7 @@ func parseCoords(line string) Coords {
 }
 
 func parseIndex(indexes []string) Index {
-	if (len(indexes) == 2) {
+	if len(indexes) == 2 {
 		x, _ := strconv.Atoi(indexes[0])
 		y, _ := strconv.Atoi(indexes[1])
 		return Index{x, y}
