400b902c546f5d98c64ed302e65048b24564ebc8
CiTuX <info@citux.ch>
2017-12-23 16:01:58 +0100

    Day 3

--- src/main/kotlin/ch/citux/aoc/day3/SpiralMemory.kt
+++ src/main/kotlin/ch/citux/aoc/day3/SpiralMemory.kt
@@ -1,36 +1,39 @@
 package ch.citux.aoc.day3
 
-import kotlin.math.abs
 import kotlin.math.ceil
-import kotlin.math.sqrt
+import kotlin.math.max
+import kotlin.math.min
 
 fun main(args: Array<String>) {
 
     val input = 368078
-    val spiralMemory = SpiralMemory(input)
-    print(spiralMemory.countStepsToCenter(input))
+    val spiralMemory = SpiralMemory(9)
+
+    var number = 0
+    var index = 1
+    while (number < input) {
+        number = spiralMemory.getValue(index++)
+    }
+    print(number)
 }
 
 data class Coordinate(val x: Int, val y: Int)
 
-class SpiralMemory(maxNumber: Int) {
-    private val size: Int
+class SpiralMemory(maxSize: Int) {
+    private val size: Int = maxSize
     private val center: Int
-    private val index: MutableMap<Int, Coordinate>
-    //    private val spiral: Array<IntArray>
-
-    private var number = 1
+    private val spiral: Array<IntArray>
+    private val index: MutableList<Int>
 
     init {
-        size = calculateSize(maxNumber)
         center = ceil(size / 2f).toInt() - 1
-        index = HashMap(maxNumber)
-//        spiral = Array(size, { _ -> IntArray(size) })
+        spiral = Array(size, { _ -> IntArray(size) })
+        index = ArrayList()
         fillArray()
     }
 
     private fun fillArray() {
-        addNumber(Coordinate(center, center), number++)
+        addNumber(Coordinate(center, center), 1)
         for (i in 1..center) {
             fillCircle(i)
         }
@@ -41,36 +44,25 @@ class SpiralMemory(maxNumber: Int) {
         val upLeft = center + distance - 1 downTo center - distance
         val downRight = center - distance + 1..center + distance
 
-        upLeft.forEach { i -> addNumber(Coordinate(i, center + distance), number++) }
-        upLeft.forEach { i -> addNumber(Coordinate(center - distance, i), number++) }
-        downRight.forEach { i -> addNumber(Coordinate(i, center - distance), number++) }
-        downRight.forEach { i -> addNumber(Coordinate(center + distance, i), number++) }
+        upLeft.forEach { i -> addNumber(Coordinate(i, center + distance)) }
+        upLeft.forEach { i -> addNumber(Coordinate(center - distance, i)) }
+        downRight.forEach { i -> addNumber(Coordinate(i, center - distance)) }
+        downRight.forEach { i -> addNumber(Coordinate(center + distance, i)) }
     }
 
-    private fun addNumber(coordinate: Coordinate, number: Int) {
-//        spiral[coordinates.x][coordinates.y] = number
-        index[number] = coordinate
-    }
-
-    private fun calculateSize(maxNumber: Int): Int {
-        val sqrt = sqrt(maxNumber.toFloat())
-        val size = ceil(sqrt).toInt()
-        return nexOddNumber(size) // size has to be odd
-    }
-
-    private fun nexOddNumber(number: Int): Int {
-        return if (number % 2 == 0) number + 1 else number
-    }
-
-    private fun manhattan(a: Coordinate?, b: Coordinate?): Int {
-        if (a != null && b != null) {
-            return abs(a.x - b.y) + abs(a.y - b.x)
+    private fun addNumber(coordinate: Coordinate, defaultNumber: Int = 0) {
+        var number = defaultNumber
+        for (x in max(coordinate.x - 1, 0)..min(coordinate.x + 1, size - 1)) {
+            for (y in max(coordinate.y - 1, 0)..min(coordinate.y + 1, size - 1)) {
+                number += spiral[x][y]
+            }
         }
-        return 0
+        spiral[coordinate.x][coordinate.y] = number
+        index.add(number)
     }
 
-    fun countStepsToCenter(number: Int): Int {
-        return manhattan(index[number], Coordinate(center, center))
+    fun getValue(position: Int): Int {
+        return index[position - 1]
     }
 }
 
--- src/test/kotlin/ch/citux/aoc/day3/SpiralMemorySpek.kt
+++ src/test/kotlin/ch/citux/aoc/day3/SpiralMemorySpek.kt
@@ -8,33 +8,40 @@ import kotlin.test.assertEquals
 
 object SpiralMemorySpek : Spek({
     given("a spiral memory") {
-        val spiralMemory = SpiralMemory(1024)
+        val spiralMemory = SpiralMemory(3)
 
         on("1") {
-            val steps = spiralMemory.countStepsToCenter(1)
-            it("Data from square 1 is carried 0 steps, since it's at the access port.") {
-                assertEquals(0, steps)
+            val steps = spiralMemory.getValue(1)
+            it("Square 1 starts with the value 1.") {
+                assertEquals(1, steps)
             }
         }
 
-        on("12") {
-            val steps = spiralMemory.countStepsToCenter(12)
-            it("Data from square 12 is carried 3 steps, such as: down, left, left.") {
-                assertEquals(3, steps)
+        on("2") {
+            val steps = spiralMemory.getValue(2)
+            it("Square 2 has only one adjacent filled square (with value 1), so it also stores 1.") {
+                assertEquals(1, steps)
             }
         }
 
-        on("23") {
-            val steps = spiralMemory.countStepsToCenter(23)
-            it("Data from square 23 is carried only 2 steps: up twice.") {
+        on("3") {
+            val steps = spiralMemory.getValue(3)
+            it("Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.") {
                 assertEquals(2, steps)
             }
         }
 
-        on("1024") {
-            val steps = spiralMemory.countStepsToCenter(1024)
-            it("Data from square 1024 must be carried 31 steps.") {
-                assertEquals(31, steps)
+        on("4") {
+            val steps = spiralMemory.getValue(4)
+            it("Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4.") {
+                assertEquals(4, steps)
+            }
+        }
+
+        on("5") {
+            val steps = spiralMemory.getValue(5)
+            it("Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.") {
+                assertEquals(5, steps)
             }
         }
     }
