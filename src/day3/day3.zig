f4cafd74d4e4e36fc0b9c2ee8cc33a6eb3b23d1f
Paul Stöhr <info@citux.ch>
2024-12-06 19:52:17 +0100

    day3: part1

--- src/day3/day3.zig
+++ src/day3/day3.zig
@@ -3,41 +3,48 @@ const regex = @import("regex");
 const Allocator = std.mem.Allocator;
 const Captures = regex.Captures;
 const Regex = regex.Regex;
+const eql = std.mem.eql;
 const expectEqual = std.testing.expectEqual;
 const parseInt = std.fmt.parseInt;
 const test_allocator = std.testing.allocator;
 
-const keyword = "mul";
+const keyword = "mul(";
 const regexPattern =
-    \\\((\d+),(\d+)\)
+    \\(\d+),(\d+)\)
 ;
 
-pub fn main() !void {}
+pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+
+    const input = @embedFile("input.txt");
+    const part1 = try multiplyNumbers(input, allocator);
+
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("{}\n", .{part1});
+}
 
 fn multiplyNumbers(input: []const u8, allocator: Allocator) !usize {
     var result: usize = 0;
     var matcher = try Regex.compile(allocator, regexPattern);
     defer matcher.deinit();
 
-    var iterator = std.mem.tokenizeAny(u8, input, keyword);
+    var iterator = std.mem.tokenizeSequence(u8, input, keyword);
 
     while (iterator.next()) |token| {
-        var c = try matcher.captures(token);
+        if (eql(u8, token, input)) break; // no valid mut found
+
+        var c = try matcher.captures(token[0..@min(8, token.len)]);
 
         if (c) |captures| {
             defer c.?.deinit();
-            var i: usize = 1; // skip the first capture
+
             var x: usize = 0;
             var y: usize = 0;
 
-            while (i < captures.len()) {
-                x = try digitAt(captures, i);
-                i += 1;
-                y = try digitAt(captures, i);
-                i += 1;
+            x = try digitAt(captures, 1);
+            y = try digitAt(captures, 2);
 
-                result += x * y;
-            }
+            result += x * y;
         }
     }
 
@@ -51,4 +58,8 @@ fn digitAt(captures: Captures, i: usize) !usize {
 test "part1" {
     const input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
     try expectEqual(161, multiplyNumbers(input, test_allocator));
+    try expectEqual(0, multiplyNumbers("mul(4*", test_allocator));
+    try expectEqual(0, multiplyNumbers("mul(6,9!", test_allocator));
+    try expectEqual(0, multiplyNumbers("?(12,34)", test_allocator));
+    try expectEqual(0, multiplyNumbers("mul ( 2 , 4 )", test_allocator));
 }
