b7b1db7b88b0f2a1e5eb3464d58b695e28cdcfdf
Paul Stöhr <info@citux.ch>
2024-12-14 20:30:32 +0100

    day4: successful test for part 1

--- src/day4/day4.ex
+++ src/day4/day4.ex
@@ -1,5 +1,82 @@
 defmodule Day4 do
-  def run(input) do
-    input |> String.length()
+  defp word(), do: "XMAS"
+  defp word_length(), do: word() |> String.length()
+
+  defp word_vectors(),
+    do: word() |> String.codepoints() |> then(&[&1, Enum.reverse(&1)])
+
+  defp window(matrix, x, y, size),
+    do: matrix |> Enum.slice(y, size) |> Enum.map(&Enum.slice(&1, x, size))
+
+  defp map_vertical(window) do
+    index_size = Enum.count(hd(window)) - 1
+
+    for y <- 0..index_size do
+      Enum.map(window, &Enum.at(&1, y))
+    end
   end
+
+  defp map_diagonal(window) do
+    index_size = Enum.count(window) - 1
+
+    for i <- 0..index_size do
+      Enum.at(window, i) |> Enum.at(i)
+    end
+  end
+
+  @spec run(binary()) :: number()
+  def run(input) do
+    matrix = input |> String.split() |> Enum.map(&String.codepoints(&1))
+    find_word(matrix)
+  end
+
+  def find_word(matrix) do
+    window_size = word_length()
+
+    find_word_vertically(matrix, window_size) +
+      find_word_horizontally(matrix, window_size) +
+      find_word_diagonally(matrix, window_size)
+  end
+
+  def find_word_vertically(matrix, window_size),
+    do:
+      matrix
+      |> Enum.chunk_every(window_size, 1, :discard)
+      |> Enum.map(&count_vertical/1)
+      |> Enum.sum()
+
+  def find_word_horizontally(matrix, window_size),
+    do:
+      matrix
+      |> Enum.map(&Enum.chunk_every(&1, window_size, 1, :discard))
+      |> Enum.map(&count_horizontal/1)
+      |> Enum.sum()
+
+  def find_word_diagonally(matrix, window_size) do
+    matrix_size = Enum.count(matrix)
+    index_size = matrix_size - window_size
+
+    find_word_diagonally(matrix, index_size, window_size) |> Enum.map(&Enum.sum(&1)) |> Enum.sum()
+  end
+
+  defp find_word_diagonally(matrix, index_size, window_size) do
+    for y <- 0..index_size do
+      for x <- 0..index_size do
+        window = window(matrix, x, y, window_size)
+        count_diagonal(window)
+      end
+    end
+  end
+
+  def count(window, vectors \\ word_vectors()),
+    do: window |> Enum.count(&Enum.member?(vectors, &1))
+
+  def count_horizontal(window),
+    do: window |> count()
+
+  def count_vertical(window),
+    do: window |> map_vertical() |> count()
+
+  def count_diagonal(window),
+    do: [map_diagonal(window), map_diagonal(Enum.reverse(window))] |> count()
 end
--- test/day4/day4_test.exs
+++ test/day4/day4_test.exs
@@ -6,4 +6,52 @@ defmodule Day4Test do
     result = Day4.run(input)
     assert result == 18
   end
+
+  test "count_horizontal" do
+    window = [
+      ["X", "M", "A", "S"],
+      ["A", "A", "M", "X"],
+      ["S", "M", "M", "X"],
+      ["S", "A", "M", "X"]
+    ]
+
+    result = Day4.count_horizontal(window)
+    assert result == 2
+  end
+
+  test "count_vertical" do
+    window = [
+      ["X", "X", "A", "S"],
+      ["S", "M", "M", "A"],
+      ["S", "A", "M", "M"],
+      ["S", "S", "M", "X"]
+    ]
+
+    result = Day4.count_vertical(window)
+    assert result == 2
+  end
+
+  test "count_diagonal normal" do
+    window = [
+      ["X", "X", "A", "S"],
+      ["S", "M", "A", "A"],
+      ["S", "M", "A", "M"],
+      ["X", "S", "M", "S"]
+    ]
+
+    result = Day4.count_diagonal(window)
+    assert result == 2
+  end
+
+  test "count_diagonal reverse" do
+    window = [
+      ["S", "X", "A", "X"],
+      ["S", "A", "M", "A"],
+      ["S", "A", "M", "M"],
+      ["S", "S", "M", "X"]
+    ]
+
+    result = Day4.count_diagonal(window)
+    assert result == 2
+  end
 end
