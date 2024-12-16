360eb028f064a9d42bc395eaedac0ebf9cc76f45
Paul Stöhr <info@citux.ch>
2024-12-16 18:38:09 +0100

    day5: part 2

--- src/day5/day5.ex
+++ src/day5/day5.ex
@@ -2,11 +2,15 @@ defmodule Day5 do
   def validated_middle_page_sum(input) do
     [rules, updates] = parse_input(input)
     valid_updates = validate_updates(updates, rules)
-    Enum.reduce(valid_updates, 0, &(get_middle_page(&1) + &2))
+    middle_page_sum(valid_updates)
   end
 
   def fixed_middle_page_sum(input) do
-    String.length(input)
+    [rules, updates] = parse_input(input)
+    valid_updates = validate_updates(updates, rules)
+    invalid_updates = updates -- valid_updates
+    fixed_updates = fix_updates(invalid_updates, rules)
+    middle_page_sum(fixed_updates)
   end
 
   defp parse_input(input) do
@@ -32,6 +36,15 @@ defmodule Day5 do
       |> Enum.with_index()
       |> Enum.all?(&validate_update?(update, elem(&1, 0), elem(&1, 1), rules))
 
+  defp fix_updates(updates, rules), do: Enum.map(updates, &fix_update(&1, rules))
+
+  defp fix_update(update, rules) do
+    Enum.sort_by(update, &Function.identity/1, &compare_pages(&1, &2, rules))
+  end
+
+  defp compare_pages(page_left, page_right, rules),
+    do: Map.get(rules, page_left, []) |> Enum.member?(page_right)
+
   defp validate_update?(update, page, index, rules) do
     rule = Map.get(rules, page)
 
@@ -43,6 +56,9 @@ defmodule Day5 do
     end
   end
 
+  defp middle_page_sum(updates),
+    do: Enum.reduce(updates, 0, &(get_middle_page(&1) + &2))
+
   defp get_middle_page(pages),
-    do: String.to_integer(Enum.at(pages, round(Enum.count(pages) / 2) - 1))
+    do: Enum.at(pages, round(Enum.count(pages) / 2) - 1) |> String.to_integer()
 end
--- src/day5/day5.exs
+++ src/day5/day5.exs
@@ -1,2 +1,3 @@
 input = __ENV__.file |> Path.dirname() |> Path.join("input.txt") |> File.read!()
 Day5.validated_middle_page_sum(input) |> IO.puts()
+Day5.fixed_middle_page_sum(input) |> IO.puts()
