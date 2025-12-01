699335e7fd29eb13bfac2c011d214ebc5f34adbe
CiTuX <info@citux.ch>
2025-12-01 14:09:48 +0100

    day1: successful test for part 1

--- src/Day01.jl
+++ src/Day01.jl
@@ -1,9 +1,46 @@
 module Day01
 
+using Match
 export part1
 
+dial = 50
+counter = 0
+
 function part1(input::String)
-    return length(input)
+    instructions = split(chomp(input), '\n')
+
+    for instruction in instructions
+        operation, distance = parseInstruction(instruction)
+        executeOperation(operation, distance)
+    end
+
+    counter
+end
+
+function parseInstruction(instruction::AbstractString)
+    direction = instruction[1]
+    distance = parse(Int, instruction[2:end])
+
+    @match direction begin
+        'L' => (-, distance)
+        'R' => (+, distance)
+    end
+end
+
+function executeOperation(operation::Function, distance::Int)
+    global dial = operation(dial, distance)
+
+    if 0 < dial < 100
+        return
+    elseif dial < 0
+        dial += 100
+    elseif dial >= 100
+        dial -= 100
+    end
+
+    if dial == 0
+        global counter += 1
+    end
 end
 
 end
