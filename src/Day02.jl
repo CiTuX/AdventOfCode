62e770e6526cafc791f0f68f2ee6709dd3da0fde
CiTuX <info@citux.ch>
2025-12-02 13:14:12 +0100

    day2: successful test for part 1

--- src/Day02.jl
+++ src/Day02.jl
@@ -1,6 +1,33 @@
 module Day02
 
-part1(input::Array) = length(input)
+part1(input::Array) = handleInput(input)
 part2(input::Array) = length(input)
+
+function handleInput(input::Array)
+    result = 0
+    ranges = split(input[1], ',')
+    for range in ranges
+        result += parseRange(range) |> validateRange
+    end
+    result
+end
+
+parseRange(range) = replace(range, "-" => ":") |> Meta.parse |> eval
+
+function validateRange(productIds::AbstractRange)
+    invalidRanges = 0
+    for productId in productIds
+        len = length("$productId")
+        if len % 2 == 0
+            base = 10^(length("$productId") / 2)
+            left = productId ÷ base
+            right = productId % base
+            if left == right
+                invalidRanges += productId
+            end
+        end
+    end
+    invalidRanges
+end
 
 end
