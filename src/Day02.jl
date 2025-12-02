313eb17bd14e1ed23f1eab6f7e8a71f6b431cb54
CiTuX <info@citux.ch>
2025-12-02 16:55:54 +0100

    day2: part 2

--- src/Day02.jl
+++ src/Day02.jl
@@ -1,33 +1,60 @@
 module Day02
 
-part1(input::Array) = handleInput(input)
-part2(input::Array) = length(input)
+part1(input::Array) = handleInput(input, validateTwice)
+part2(input::Array) = handleInput(input, validateMany)
 
-function handleInput(input::Array)
+function handleInput(input::Array, validationFunction::Function)
     result = 0
     ranges = split(input[1], ',')
     for range in ranges
-        result += parseRange(range) |> validateRange
+        result += validate(range, validationFunction)
     end
     result
 end
 
-parseRange(range) = replace(range, "-" => ":") |> Meta.parse |> eval
+parseRange(range::AbstractString) = replace(range, "-" => ":") |> Meta.parse |> eval
 
-function validateRange(productIds::AbstractRange)
+function validate(range::AbstractString, validationFunction::Function)
     invalidRanges = 0
+    productIds = parseRange(range)
     for productId in productIds
-        len = length("$productId")
-        if len % 2 == 0
-            base = 10^(length("$productId") / 2)
-            left = productId ÷ base
-            right = productId % base
-            if left == right
-                invalidRanges += productId
+        invalidRanges += validationFunction(productId)
+    end
+    invalidRanges
+end
+
+function validateTwice(productId::Int)
+    len = length("$productId")
+    if len % 2 == 0
+        base = 10^(length("$productId") / 2)
+        left = productId ÷ base
+        right = productId % base
+        if left == right
+            return productId
+        end
+    end
+    return 0
+end
+
+function validateMany(productId::Int)
+    len = length("$productId")
+    for iteration in range(1, len ÷ 2)
+        if len % iteration == 0
+            parts = len ÷ iteration
+            base = 10^(len - iteration)
+            number = productId ÷ base
+            repeated = 0
+
+            for _ in range(; stop=parts)
+                repeated = (repeated * 10^iteration) + number
+            end
+
+            if repeated == productId
+                return productId
             end
         end
     end
-    invalidRanges
+    return 0
 end
 
 end
