c1082f9e340ca253c8e16633d266db4efba3f721
CiTuX <info@citux.ch>
2025-12-02 11:28:04 +0100

    Add args support to choose day

--- src/AdventOfCode.jl
+++ src/AdventOfCode.jl
@@ -1,13 +1,24 @@
 module AdventOfCode
 
-export main, Day01
+export main
 
 include("Day01.jl")
 
 function (@main)(args)
-    input = readlines("res/day01.txt")
-    println("Part1: $(Day01.part1(input))")
-    println("Part2: $(Day01.part2(input))")
+    prefix = if length(args) == 0
+        ""
+    elseif length(args) == 1
+        lpad(args[1], 2, "0")
+    end
+
+    days = names(AdventOfCode, all=true) |> filter(name -> startswith("$name", "Day$prefix"))
+    
+    for day in days
+        input = readlines("res/$(lowercase("$day")).txt")
+        modul = eval(day)
+        println("$day Part1: $(modul.part1(input))")
+        println("$day Part2: $(modul.part2(input))")
+    end
 end
 
 end
