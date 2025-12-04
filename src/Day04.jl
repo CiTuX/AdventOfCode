83b999f82fcf11abd21c70f4e50fdcbbf06c7fa2
CiTuX <info@citux.ch>
2025-12-04 16:27:29 +0100

    day4: part 2

--- src/Day04.jl
+++ src/Day04.jl
@@ -9,15 +9,28 @@ const searchKernel = [
 ]
 
 part1(input::Array) = handleInput(input)
-part2(input::Array) = length(input)
+part2(input::Array) = handleInput(input, 1000)
 
-function handleInput(input::Array)
-    gridSize = length(input)
+function handleInput(input::Array, iterations::Int=1)
+    total = 0
     grid = parseInput(input)
+    for _ in 1:iterations
+        removed, grid = removeRolls(grid)
+        if removed == 0
+            break
+        end
+        total += removed
+    end
+    total
+end
+
+function removeRolls(grid::AbstractArray)
+    gridSize = size(grid, 1)
     convolution = conv(grid, searchKernel)
     neighbors = convolution[2:gridSize+1, 2:gridSize+1]
-    filtered = map((neighbor, active) -> active && neighbor < 4, neighbors, grid)
-    sum(filtered)
+    filtered = map((active, neighbor) -> active == 1 && neighbor < 4, grid, neighbors)
+    count = sum(filtered)
+    (count, grid - filtered)
 end
 
 function parseInput(input::Array)
