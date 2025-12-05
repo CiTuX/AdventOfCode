22c7977a61d42dc24c4a01a38c59be245c35a49e
CiTuX <info@citux.ch>
2025-12-05 14:33:51 +0100

    day5: part 2

--- src/Day05.jl
+++ src/Day05.jl
@@ -1,18 +1,24 @@
 module Day05
 
 part1(input::Array) = handleInput(input)
-part2(input::Array) = length(input)
+part2(input::Array) = handleInput(input, countAllFreshIngredients)
 
-function handleInput(input::Array)
+function handleInput(input::Array, action::Function=countAvailableFreshIngredients)
     splitIndex = findfirst(isempty, input)
-    freshRanges = input[1:splitIndex-1] |> parseRanges
-    ingredientIds = input[splitIndex+1:end] |> parseIngredients
+    freshRanges = input[1:splitIndex-1]
+    ingredientIds = input[splitIndex+1:end]
 
+    action(freshRanges, ingredientIds)
+end
+
+function countAvailableFreshIngredients(freshRanges::Array, ingredientIds::Array)
+    ranges = map(parseRange, freshRanges)
+    ids = ingredientIds |> parseIngredients
     freshIds = 0
-    for ingredientId in ingredientIds
-        for range in freshRanges
-            if ingredientId in range
-                freshIds+=1
+    for id in ids
+        for range in ranges
+            if id in range
+                freshIds += 1
                 break
             end
         end
@@ -20,8 +26,23 @@ function handleInput(input::Array)
     freshIds
 end
 
-parseRanges(ranges::Array) = map(parseRange, ranges)
+function countAllFreshIngredients(freshRanges::Array, _::Array)
+    ranges = map(parseTuple, freshRanges) |> sort
+    merged = []
+    for range in ranges
+        if isempty(merged) || range[1] > merged[end][2] + 1
+            push!(merged, range)
+        else
+            merged[end] = (merged[end][1], max(merged[end][2], range[2]))
+        end
+    end
+    mergedRanges = [u:v for (u, v) in merged]
+    map(length, mergedRanges) |> sum
+end
+
+
 parseRange(range::AbstractString) = replace(range, "-" => ":") |> Meta.parse |> eval
+parseTuple(range::AbstractString) = "($(replace(range, "-" => ",")))" |> Meta.parse |> eval
 parseIngredients(ingredients::Array) = map(ingredient -> parse(Int, ingredient), ingredients)
 
 end
