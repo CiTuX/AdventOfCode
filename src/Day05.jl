1ab24e4ccc8e0bb580e9c0d9bc1a918be092cf27
CiTuX <info@citux.ch>
2025-12-05 10:38:31 +0100

    day5: successful test for part 1

--- src/Day05.jl
+++ src/Day05.jl
@@ -1,6 +1,27 @@
 module Day05
 
-part1(input::Array) = length(input)
+part1(input::Array) = handleInput(input)
 part2(input::Array) = length(input)
+
+function handleInput(input::Array)
+    splitIndex = findfirst(isempty, input)
+    freshRanges = input[1:splitIndex-1] |> parseRanges
+    ingredientIds = input[splitIndex+1:end] |> parseIngredients
+
+    freshIds = 0
+    for ingredientId in ingredientIds
+        for range in freshRanges
+            if ingredientId in range
+                freshIds+=1
+                break
+            end
+        end
+    end
+    freshIds
+end
+
+parseRanges(ranges::Array) = map(parseRange, ranges)
+parseRange(range::AbstractString) = replace(range, "-" => ":") |> Meta.parse |> eval
+parseIngredients(ingredients::Array) = map(ingredient -> parse(Int, ingredient), ingredients)
 
 end
