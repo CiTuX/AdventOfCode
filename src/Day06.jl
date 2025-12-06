c0541b4cbf27542eda4ed2b44081f9a22bcaa89d
CiTuX <info@citux.ch>
2025-12-06 10:41:42 +0100

    day6: successful test for part 1

--- src/Day06.jl
+++ src/Day06.jl
@@ -4,7 +4,19 @@ part1(input::Array) = handleInput(input)
 part2(input::Array) = handleInput(input)
 
 function handleInput(input::Array)
-    length(input)
+    input = map(split, input)
+    operations = parseOperations(pop!(input))
+    numbers = parseNumbers(input)
+    numbers = vcat(numbers'...)'
+    results = []
+    for (i, row) in enumerate(eachrow(numbers))
+        push!(results, reduce(operations[i], row))
+    end
+    sum(results)
 end
+
+parseOperations(operations::Array) = map(operation -> operation |> Meta.parse |> eval, operations)
+parseNumbers(input::Array) = map(parseInts, input)
+parseInts(row::Array) = parse.(Int, row)
 
 end
