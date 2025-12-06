de8c4d18fb869f77a98d43406a41eff157ce803d
CiTuX <info@citux.ch>
2025-12-06 08:50:47 +0100

    day6: add test for part 1

---
+++ src/Day06.jl
@@ -0,0 +1,10 @@
+module Day06
+
+part1(input::Array) = handleInput(input)
+part2(input::Array) = handleInput(input)
+
+function handleInput(input::Array)
+    length(input)
+end
+
+end
---
+++ test/Day06.jl
@@ -0,0 +1,11 @@
+using AdventOfCode.Day06
+
+const input = split("""
+123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  """, '\n')
+
+@testset "Day06 Tests" begin
+    @test Day06.part1(input) == 4277556
+end
