8fd075577482960f507a0e9b4d180e7a5e49af74
CiTuX <info@citux.ch>
2025-12-07 15:33:17 +0100

    day7: successful test for part 1

--- src/Day07.jl
+++ src/Day07.jl
@@ -1,10 +1,39 @@
 module Day07
 
-part1(input::Array) = handleInput(input)
-part2(input::Array) = handleInput(input)
+const startmarker = 'S'
+const splittermarker = '^'
 
-function handleInput(input::Array)
-    length(input)
+part1(input::Array) = handle_input(input)
+part2(input::Array) = handle_input(input)
+
+function handle_input(input::Array)
+    tachyons = BitMatrix(undef, 1, length(input[1]))
+    counter = 0
+    for line in input
+        if sum(tachyons) == 0
+            startIndex = findfirst(startmarker, line)
+            tachyons[startIndex] = true
+        else
+            counter += collision_detection!(tachyons, line)
+        end
+    end
+    counter
+end
+
+function collision_detection!(tachyons::BitMatrix, line::AbstractString)
+    counter = 0
+    if contains(line, splittermarker)
+        splitters = findall(splittermarker, line)
+        for splitter in splitters
+            if tachyons[splitter]
+                tachyons[splitter] = false
+                tachyons[splitter-1] = true
+                tachyons[splitter+1] = true
+                counter += 1
+            end
+        end
+    end
+    counter
 end
 
 end
