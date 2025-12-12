c15e3024ec8df8544ae0a06b4260647d0b595215
CiTuX <info@citux.ch>
2025-12-12 10:51:14 +0100

    day7: part 2

--- src/Day07.jl
+++ src/Day07.jl
@@ -3,10 +3,10 @@ module Day07
 const startmarker = 'S'
 const splittermarker = '^'
 
-part1(input::Array) = handle_input(input)
-part2(input::Array) = handle_input(input)
+part1(input::Array) = count_splits(input)
+part2(input::Array) = count_timelines(input)
 
-function handle_input(input::Array)
+function count_splits(input::Array)
     tachyons = BitMatrix(undef, 1, length(input[1]))
     counter = 0
     startIndex = findfirst(startmarker, input[1])
@@ -15,6 +15,30 @@ function handle_input(input::Array)
         counter += collision_detection!(tachyons, line)
     end
     counter
+end
+
+function count_timelines(input::Array)
+    rows = length(input)
+    cols = length(input[1])
+    timelines = zeros(Int, rows, cols)
+    startIndex = findfirst(startmarker, input[1])
+    timelines[1, startIndex] = 1
+
+    for row in 2:rows
+        for col in 1:cols
+            if input[row][col] == splittermarker
+                if col > 1
+                    timelines[row, col-1] += timelines[row-1, col]
+                end
+                if col < cols
+                    timelines[row, col+1] += timelines[row-1, col]
+                end
+            else
+                timelines[row, col] += timelines[row-1, col]
+            end
+        end
+    end
+    sum(timelines[end, :])
 end
 
 function collision_detection!(tachyons::BitMatrix, line::AbstractString)
