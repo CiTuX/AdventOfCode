c8cc8172af0b28ed43d7d186ae075f1ad04aa851
CiTuX <info@citux.ch>
2025-12-12 16:35:32 +0100

    day8: parse input

--- src/Day08.jl
+++ src/Day08.jl
@@ -4,7 +4,11 @@ part1(input::Array) = handle_input(input)
 part2(input::Array) = handle_input(input)
 
 function handle_input(input::Array)
-    length(input)
+    input = parse_input(input)
+    display(input)
 end
+
+parse_input(input::Array) = parse_ints.(split.(input,','))
+parse_ints(row::Array) = parse.(Int, row)
 
 end
